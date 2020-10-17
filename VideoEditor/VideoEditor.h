b6e673de85cc47fd5c4615c0ec9aef0c4b6bb13c
levantAJ <sirlevantai@gmail.com>
2020-10-17 16:50:52 +0700

    Add git

---
+++ VideoEditor/VideoEditor.h
@@ -0,0 +1,18 @@
+//
+//  VideoEditor.h
+//  VideoEditor
+//
+//  Created by Tai Le on 10/17/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VideoEditor.
+FOUNDATION_EXPORT double VideoEditorVersionNumber;
+
+//! Project version string for VideoEditor.
+FOUNDATION_EXPORT const unsigned char VideoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoEditor/PublicHeader.h>
+
+
